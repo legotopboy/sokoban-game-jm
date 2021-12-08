7a214fd712299fdda93901c52cac6f319017816f
junmyeon <legotopboy@naver.com>
2021-12-08 15:39:49 +0900

    Main과 소코반 Map을 관리하는 MapManager 분리

--- src/MapManager.java
+++ src/MapManager.java
@@ -1,6 +1,14 @@
+import java.io.IOException;
+
 public class MapManager {
 
-    public static void main(String[] args) {
+    public void init() throws IOException {
+
+
+        String stage1 = "Stage 1\n#####\n#OoP#\n#####\n=====\n" +
+                "Stage 2\n  #######  \n###  O  ###\n#    o    #\n# Oo P oO #\n###  o  ###\n #   O  #  \n ########  \n===";
+
+        //scanInputData(stage1);
 
     }
 }
